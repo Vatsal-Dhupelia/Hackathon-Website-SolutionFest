72ff65821b10c97f1cadde1d18a2cb6ae9164eab
Vatsal Dhupelia <85610288+Vatsal-Dhupelia@users.noreply.github.com>
2022-02-09 01:04:53 +0530

    Homepage Further Implementation

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import vectorimg1 from "../../images/vector1.jpg"
+import vectorimg2 from "../../images/vector2.png"
+import vectorimg3 from "../../images/vector3.jpg"
+import vectorimg4 from "../../images/vector4.jpg"
 import "./Home.css"
 
 const Home = () => {
@@ -29,8 +32,42 @@ const Home = () => {
           title='video'
         />
       </div>
+      <div id="home-desc" class="home-desc">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="titlepage">
+                <h1> Heading </h1>
+              </div>
+              <div class="row">
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                  <div id="box" class="desc-box1">
+                    <i><img src={vectorimg2} alt="#" /></i>
+                    <h2>Think</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quam delectus id ex, minus similique suscipit est ullam aliquam ipsum debitis placeat consequatur in aut facere, adipisci magnam dolorem ok.</p>
+                  </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                  <div id="box" class="desc-box2">
+                    <i><img id='desc-box1' src={vectorimg3} alt="#" /></i>
+                    <h2>Code</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa quo maxime aliquid inventore consectetur tempore doloremque placeat. Voluptatem porro amet error debitis, numquam cupiditate sit tempore voluptatum optio odit possimus!</p>
+                  </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                  <div id="box" class="desc-box3">
+                    <i><img src={vectorimg4} alt="#" /></i>
+                    <h2>Execute</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur voluptas aliquam odit vel hic saepe quibusdam a quisquam officia laborum pariatur reprehenderit sunt adipisci voluptates ut quaerat iusto, commodi corrupti.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  );
+);
 };
 
-export default Home;
+  export default Home;
