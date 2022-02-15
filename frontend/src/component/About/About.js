a9b60c42cc412927e27ff19db9640e3cc7d697f9
Smit <smitbhansali7@gmail.com>
2022-02-15 20:54:21 +0530

    about and timeline page done and navbar edited

--- frontend/src/component/About/About.js
+++ frontend/src/component/About/About.js
@@ -14,7 +14,7 @@ const About = () => {
                     </div>
                     <div className="abt-desc">
                         <p>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis laboriosam incidunt, neque ut hic voluptatem id nostrum eveniet error ducimus quisquam. Accusantium vel expedita vitae ea ut praesentium vero quidem harum assumenda ullam itaque incidunt. Lorem ipsum dolor sit amet consectetur !
+                            GDSC-BVM’s Event-name is a mini hackathon/mini version of Google’s Solution Challenge 2022 which basically invites and encourages students from all over the campus to build a solution which matches and aims to solve UN’s 17 Sustainable Development problems. We are motivating participants to learn new technologies and create solutions for the real-life world and explore your imagination and develop your skills. It is a learn & solve or solve & learn type of hackathon.
                         </p>
                     </div>
 
@@ -31,12 +31,12 @@ const About = () => {
                     <img src={vectorimg2} alt="" />
                 </div>
                 <div id='abt-content1' className="content1">
-                    <div className="about-head3">
-                        <h2>Heading</h2>
+                    <div className="about-head3 my-3">
+                        <h2>United Nations 17 Sustainable Development Goals</h2>
                     </div>
                     <div className="abt-desc1">
                         <p>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis laboriosam incidunt, neque ut hic voluptatem id nostrum eveniet error ducimus quisquam. Accusantium vel expedita vitae ea ut praesentium vero quidem harum assumenda ullam itaque incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, suscipit. Laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Created by the United Nations in 2015, with support from all 193 United Nations Member States, the 17 Sustainable Development Goals aim to end poverty, ensure prosperity, and protect the planet.
                         </p>
                     </div>
                 </div>
@@ -44,11 +44,11 @@ const About = () => {
             <div className="abt-container">
                 <div className="content1">
                     <div id='abt-head3' className="about-head3">
-                        <h2>Heading</h2>
+                        <h2>Goal</h2>
                     </div>
                     <div id='about-desc1' className="abt-desc">
                         <p>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis laboriosam incidunt, neque ut hic voluptatem id nostrum eveniet error ducimus quisquam. Accusantium vel expedita vitae ea ut praesentium vero quidem harum assumenda ullam itaque incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, doloremque.
+                            The goal of a hackathon is to create functioning software or hardware by the end of the event. Hackathons tend to have a specific focus, which can include the programming languages used, the Operating system, an application, an API, or the subject and the demographic group of the programmers. In other cases, there is no restriction on the type of software being created. Hackathon is to create optimal solutions for particular problems.
                         </p>
                     </div>
                 </div>
@@ -66,15 +66,15 @@ const About = () => {
                             <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12">
                                 <div id="box" className="desc-box">
                                     <i><img src={vectorimg1} alt="#" /></i>
-                                    <h2>Name</h2>
-                                    <p>Description</p>
+                                    <h2>Vatsal Sir</h2>
+                                    <p>GDSC Mentor and Assistant Professor</p>
                                 </div>
                             </div>
                             <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12">
                                 <div id="box" className="desc-box">
                                     <i><img id='desc-box1' src={vectorimg1} alt="#" /></i>
-                                    <h2>Name</h2>
-                                    <p>Description</p>
+                                    <h2>Priyank Sir</h2>
+                                    <p>Assistant Professor</p>
                                 </div>
                             </div>
                             <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12">
--- frontend/src/component/footer/Footer.js
+++ frontend/src/component/footer/Footer.js
@@ -7,10 +7,10 @@ const Footer = () => {
         <footer id='footer'>
             <p>CATCH US ON</p>
             <div className="leftFooter">
-                <a href="#" className="fa fa-facebook"></a>
-                <a href="#" className="fa fa-youtube"></a>
-                <a href="#" className="fa fa-instagram"></a>
+                <a href="https://linktr.ee/GDSC_BVM" className='fa fa-tree'></a>
             </div>
+            <p>Prathviraj: <a href="tel:+917226001839">+91 72260 01839</a></p>
+            <p>Manav: <a href="tel:+919157441707">+91 91574 41707</a></p>
             <div className="midFooter">
                 <p>Copyright 2022 All rights reserved | SolutionFest</p>
             </div>
